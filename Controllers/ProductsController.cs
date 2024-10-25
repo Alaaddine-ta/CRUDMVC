93c2a5fc082b24af6f64cc658f01ab3d6e6dc86c
Alaaddine-ta <alae.tabet02@gmail.com>
2024-10-25 16:24:18 +0200

    add the product controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using CRUDMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
